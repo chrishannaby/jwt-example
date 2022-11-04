d53c849b1108b363e9adc359f515a80829ef9ea2
Chris Hannaby <chris.hannaby@netlify.com>
2022-11-04 16:22:45 -0600

    remove new lines

--- nginx/netlify/functions/nginx.js
+++ nginx/netlify/functions/nginx.js
@@ -1,9 +1,8 @@
 import fetch from "node-fetch";
 
 const proxyUrl = "stunning-gingersnap-124357.netlify.app";
-const jwt = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-    .eyJhcHBfbWV0YWRhdGEiOnsiYXV0aG9yaXphdGlvbiI6eyJyb2xlcyI6WyJuZ2lueCJdfX19
-    .Pvt6cYY6QMDb18ab2cuyHC9BH9EzyHm0PgvgIsa1P9c`;
+const jwt =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfbWV0YWRhdGEiOnsiYXV0aG9yaXphdGlvbiI6eyJyb2xlcyI6WyJuZ2lueCJdfX19.Pvt6cYY6QMDb18ab2cuyHC9BH9EzyHm0PgvgIsa1P9c";
 
 export async function handler(event, context) {
   console.log(event);
