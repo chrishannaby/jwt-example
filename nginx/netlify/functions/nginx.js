15d7efa1f88a32a75407f06c29d120036377c06b
chrishannaby <chris.hannaby@netlify.com>
2023-01-13 18:22:49 -0700

    Update nginx.js

--- nginx/netlify/functions/nginx.js
+++ nginx/netlify/functions/nginx.js
@@ -5,10 +5,9 @@ const jwt =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIxNDc0ODM2NDcsImFwcF9tZXRhZGF0YSI6eyJhdXRob3JpemF0aW9uIjp7InJvbGVzIjpbImFkbWluIl19fX0.QrtxkldV2FmLi1jVC7mE93LSZ2muz2YKQdixQsYp9fo";
 
 export async function handler(event, context) {
-  console.log(event);
   const response = await fetch(`https://${proxyUrl}${event.path}`, {
     headers: {
-      Authorization: `jwt ${jwt}`,
+      "X-NF-User-Token": jwt,
     },
   });
   const body = await response.text();
