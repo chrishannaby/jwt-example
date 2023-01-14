533fbbfd1dfd56bac7cbb9c9cd5a55fd84ce6087
chrishannaby <chrishannaby@gmail.com>
2023-01-13 18:43:02 -0700

    try 64 bit epoch

--- nginx/netlify/functions/nginx.js
+++ nginx/netlify/functions/nginx.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 const proxyUrl = "secret-site-that-needs-jwt-to-access.netlify.app";
-const cookie = "nf_jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIxNDc0ODM2NDcsImFwcF9tZXRhZGF0YSI6eyJhdXRob3JpemF0aW9uIjp7InJvbGVzIjpbImFkbWluIl19fX0.QrtxkldV2FmLi1jVC7mE93LSZ2muz2YKQdixQsYp9fo; Secure; HttpOnly;"
+const cookie = "nf_jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjI5MjI3NzAyNjU5NiwiYXBwX21ldGFkYXRhIjp7ImF1dGhvcml6YXRpb24iOnsicm9sZXMiOlsiYWRtaW4iXX19fQ.SBIBVT5zeu6JzToowIZvzomx5-BOzFMhWT8NiSg9q1A; Secure; HttpOnly;"
 
 export async function handler(event, context) {
   const response = await fetch(`https://${proxyUrl}${event.path}`, {
