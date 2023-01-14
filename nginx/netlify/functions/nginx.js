6026b32ceaa66f01569e3f41359b2890c536a466
chrishannaby <chris.hannaby@netlify.com>
2023-01-13 18:27:53 -0700

    Update nginx.js

--- nginx/netlify/functions/nginx.js
+++ nginx/netlify/functions/nginx.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-const proxyUrl = "stunning-gingersnap-124357.netlify.app";
+const proxyUrl = "secret-site-that-needs-jwt-to-access.netlify.app";
 const jwt =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIxNDc0ODM2NDcsImFwcF9tZXRhZGF0YSI6eyJhdXRob3JpemF0aW9uIjp7InJvbGVzIjpbImFkbWluIl19fX0.QrtxkldV2FmLi1jVC7mE93LSZ2muz2YKQdixQsYp9fo";
 
