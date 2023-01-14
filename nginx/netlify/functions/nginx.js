06195093862270a39e5ee0240074e906897ce068
chrishannaby <chris.hannaby@netlify.com>
2023-01-13 18:25:19 -0700

    Update nginx.js

--- nginx/netlify/functions/nginx.js
+++ nginx/netlify/functions/nginx.js
@@ -4,10 +4,12 @@ const proxyUrl = "stunning-gingersnap-124357.netlify.app";
 const jwt =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIxNDc0ODM2NDcsImFwcF9tZXRhZGF0YSI6eyJhdXRob3JpemF0aW9uIjp7InJvbGVzIjpbImFkbWluIl19fX0.QrtxkldV2FmLi1jVC7mE93LSZ2muz2YKQdixQsYp9fo";
 
+const cookie = "nf_jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIxNDc0ODM2NDcsImFwcF9tZXRhZGF0YSI6eyJhdXRob3JpemF0aW9uIjp7InJvbGVzIjpbImFkbWluIl19fX0.QrtxkldV2FmLi1jVC7mE93LSZ2muz2YKQdixQsYp9fo; Expires=Thu, 21 Oct 2023 07:28:00 GMT; Secure; HttpOnly; Domain=stunning-gingersnap-124357.netlify.app; Path=/;"
+
 export async function handler(event, context) {
   const response = await fetch(`https://${proxyUrl}${event.path}`, {
     headers: {
-      "X-NF-User-Token": jwt,
+      cookie: cookie
     },
   });
   const body = await response.text();
